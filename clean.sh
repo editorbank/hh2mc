1ff42017d5c0efe8bdc4ecabab7c816562a3ffc3
editornank <>
2023-06-04 04:32:06 +0300

    change clean.sh

--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
 #!/bin/env bash
 source $(dirname $0)/config.sh
+cd $(dirname $0)
 
-[ ! -z "$($docker ps -q -a -f name=$docker_container)" ] && $docker rm -f $docker_container
-[ ! -z "$($docker images -q -f reference=$docker_image)" ] && $docker rmi -f $docker_image
+# Удаление контейнеров и образов сделанных проектом
+$docker ps -q -a -f name=$docker_container | xargs -r $docker rm -f
+$docker images -q -f reference=$docker_image | xargs -r $docker rmi -f
 
-for i in $(git ls-files --others --ignored --exclude-standard|grep -v .bak); do
-  rm -v $i
-done
+# Удаление игнорируемых Git-ом файлов кроме *.bak (как файлов так и директорий)
+git ls-files --others --ignored --exclude-standard | grep -v .bak | xargs -r rm 
 
+# Удаление только пустых директорий
+find . -empty -type d -delete
